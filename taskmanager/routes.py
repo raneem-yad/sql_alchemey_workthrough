7842ea2d4137a200a768215ce3bc604d733d4db9
raneem-yad <eng.raneem.yad@gmail.com>
2024-03-18 11:02:55 +0100

    add new tasks form and backend

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template , request , redirect, url_for
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
+
 
 # db.create_all()
 @app.route("/")
@@ -12,12 +13,12 @@ def home():
 def categories():
     # we converted to list because it returns a cursor
     categories_from_db = list(Category.query.order_by(Category.category_name).all())
-    return render_template("categories.html", categories= categories_from_db)
+    return render_template("categories.html", categories=categories_from_db)
 
 
-@app.route("/add_category", methods=["GET","POST"])
+@app.route("/add_category", methods=["GET", "POST"])
 def add_category():
-    if request.method == "POST" :
+    if request.method == "POST":
         category = Category(category_name=request.form.get("category_name"))
         db.session.add(category)
         db.session.commit()
@@ -33,3 +34,28 @@ def edit_category(category_id):
         db.session.commit()
         return redirect(url_for("categories"))
     return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    return redirect(url_for("categories"))
+
+
+@app.route("/add_task", methods=["GET", "POST"])
+def add_task():
+    categories_db = list(Category.query.order_by(Category.category_name).all())
+    if request.method == "POST":
+        task = Task(
+            task_name=request.form.get("task_name"),
+            task_description=request.form.get("task_description"),
+            is_urgent=bool(True if request.form.get("is_urgent") else False),
+            due_date=request.form.get("due_date"),
+            category_id=request.form.get("category_id")
+        )
+        db.session.add(task)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add_task.html", categories=categories_db)
