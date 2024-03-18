90d63aa7c9758f4389cd521c11069302edd2888e
raneem-yad <eng.raneem.yad@gmail.com>
2024-03-18 11:11:07 +0100

    show tasks

--- taskmanager/static/js/scripts.js
+++ taskmanager/static/js/scripts.js
@@ -14,4 +14,8 @@ document.addEventListener("DOMContentLoaded", function() {
     // select initialization
     let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
+
+    // collapsible initializataion
+    let collapsible = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsible);
 });
